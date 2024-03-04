c01805e030f6223b193846d1e12cbac2dbbe540e
Nikola Shalamanov <nikolashalamanov999@gmail.com>
2024-03-04 14:15:32 +0200

    ServiceCollection added to simplify Program.cs

---
+++ ProjectSoftUni2024/Extentions/ServiceCollectionExtention.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ProjectSoftUni2024.Data;
+using System.Runtime.CompilerServices;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    static public class ServiceCollectionExtention
+    {
+        static public IServiceCollection AddApplicationServices(this IServiceCollection services)
+
+        {
+            return services;
+        }
+
+
+        static public IServiceCollection AddApplicationDbContext(this IServiceCollection services, IConfiguration config)
+
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            return services;
+        }
+
+        static public IServiceCollection AddApplicationIdentity(this IServiceCollection services, IConfiguration config)
+
+        {
+
+            services
+                .AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            return services;
+        }
+
+    }
+}
